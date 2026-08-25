7a7fb2df20a7823114b946587d08de24274e55a8
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2026-08-25 15:54:24 +0300

    add

---
+++ service/refreshToken.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export const getNewAccessToken = async () => {
+  const cookieStore = await cookies();
+
+  const refreshToken = cookieStore.get("refreshToken")?.value;
+
+  if (!refreshToken) {
+    return {
+      success: false,
+      message: "Refresh token not found",
+    };
+  }
+
+  const response = await fetch(
+    `${process.env.BACKEND_APP_URL}/api/auth/refreshToken`,
+    {
+      method: "POST",
+      headers: {
+        Cookie: `refreshToken=${refreshToken}`,
+      },
+      cache: "no-store",
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+};
