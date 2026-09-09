d478229ab83aa04a8e29d2b40e02c91fc95a1316
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2026-09-09 10:42:55 +0600

    added

--- app/(authGroup)/_action/auth.ts
+++ app/(authGroup)/_action/auth.ts
@@ -1,7 +1,13 @@
-
-"use server";
+ "use server";
 
 export async function registerAction(formData: FormData) {
+  const photo = formData.get("photo");
+
+  // Empty file হলে FormData থেকে remove করবে
+  if (photo instanceof File && photo.size === 0) {
+    formData.delete("photo");
+  }
+
   try {
     const response = await fetch(
       `${process.env.BACKEND_API_URL}/api/auth/register`,
@@ -25,12 +31,12 @@ export async function registerAction(formData: FormData) {
       message: result.message || "Registration successful",
     };
   } catch (error) {
-    console.error("Registration Error:", error);
+    console.error("Registration error:", error);
 
     return {
       success: false,
-      message: "Something went wrong. Please try again.",
+      message: "Something went wrong",
     };
   }
 }
-
+ 
