fca8a70bb5bca147201528f25fdb616606deaaef
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2026-09-09 10:36:17 +0600

    added

--- app/(authGroup)/_action/auth.ts
+++ app/(authGroup)/_action/auth.ts
@@ -1,33 +1,36 @@
+
 "use server";
 
-type RegisterPayload = {
-  name: string;
-  email: string;
-  password: string;
-  role: "CUSTOMER" | "PROVIDER";
-};
-
-// export async function registerAction(payload: RegisterPayload) {
 export async function registerAction(formData: FormData) {
-  const response = await fetch(
-    `${process.env.BACKEND_API_URL}/api/auth/register`,
-    {
-      method: "POST",
-      body: formData
+  try {
+    const response = await fetch(
+      `${process.env.BACKEND_API_URL}/api/auth/register`,
+      {
+        method: "POST",
+        body: formData,
+      },
+    );
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      return {
+        success: false,
+        message: result.message || "Registration failed",
+      };
     }
-  );
 
-  const result = await response.json();
+    return {
+      success: true,
+      message: result.message || "Registration successful",
+    };
+  } catch (error) {
+    console.error("Registration Error:", error);
 
-  if (!response.ok) {
     return {
       success: false,
-      message: result.message || "Registration failed",
+      message: "Something went wrong. Please try again.",
     };
   }
-
-  return {
-    success: true,
-    message: result.message || "Registration successful",
-  };
 }
+
