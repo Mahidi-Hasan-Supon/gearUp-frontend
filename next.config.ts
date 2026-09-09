40c9355b5f59e6f78f8bb69a4751e5efd72153fc
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2026-09-09 12:00:17 +0600

    added

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,42 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "encrypted-tbn0.gstatic.com" },
-      { protocol: "https", hostname: "png.pngtree.com" },
-      { protocol: "https", hostname: "example.com" },
-      { protocol: "https", hostname: "res.cloudinary.com" },
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      // {
+      //   protocol: "https",
+      //   hostname: "encrypted-tbn0.gstatic.com",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "png.pngtree.com",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "example.com",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "res.cloudinary.com",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "lh3.googleusercontent.com",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "static.vecteezy.com",
+      // },
     ],
   },
-  experimental: { serverActions: { bodySizeLimit: "10mb" } },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
