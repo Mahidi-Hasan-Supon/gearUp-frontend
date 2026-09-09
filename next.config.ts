ccc3bddf60efb4cfeaac512048a774c837e19e2a
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2026-09-09 11:08:53 +0600

    added

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
     ],
   },
+  experimental: { serverActions: { bodySizeLimit: "10mb" } },
 };
 
 export default nextConfig;
